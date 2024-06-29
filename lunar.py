9f4a9f36f1b9fad87df28af5f0c553735472bfd7
tntgamer685347 <115848136+tntgamer685347@users.noreply.github.com>
2024-06-30 01:29:36 +0200

    Update lunar.py

--- lunar.py
+++ lunar.py
@@ -41,7 +41,7 @@ def setup():
     targeting_sens = prompt("Targeting Sensitivity (from in-game settings): ")
 
     print("[INFO] Your in-game targeting sensitivity must be the same as your scoping sensitivity")
-    sensitivity_settings = {"xy_sens": xy_sens, "targeting_sens": targeting_sens, "xy_scale": 10/xy_sens, "targeting_scale": 340/(targeting_sens * xy_sens), "targeting_scale_aiming": 960/(targeting_sens * xy_sens)}
+    sensitivity_settings = {"xy_sens": xy_sens, "targeting_sens": targeting_sens, "xy_scale": 10/xy_sens, "targeting_scale": 335/(targeting_sens * xy_sens), "targeting_scale_aiming": 955/(targeting_sens * xy_sens)}
 
     with open('lib/config/config.json', 'w') as outfile:
         json.dump(sensitivity_settings, outfile)
